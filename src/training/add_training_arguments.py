ad33cd6c79cc7d4b7264f2d5a21d10cea9c9739c
Ivan Goldov <ivan.goldov@pm.me>
2021-12-12 14:09:18 +0300

    Fix argparser

--- src/training/add_training_arguments.py
+++ src/training/add_training_arguments.py
@@ -9,7 +9,6 @@ def add_training_arguments(parser: ArgumentParser):
     parser.add_argument('--num_workers', type=int, default=16)
     parser.add_argument('--n_channels', help='number of channels of images', type=int, default=3)
     parser.add_argument('--hidden_size', help='size of channels of hidden representation', type=int, default=256)
-    parser.add_argument('--test_batch_size', type=int, default=16)
     parser.add_argument('--wandb_login', help='login for wandb to log process', type=str, default=None)
     parser.add_argument('--save_path', help='path to save model', type=str, default=None)
     parser.add_argument('--seed', help='fix random seed', type=int, default=0)
